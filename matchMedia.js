13a7dc4b015204a005133b55bbe9438590f4ce80
Jonathan Rupp <jorupp@gmail.com>
2015-03-18 11:10:12 -0500

    Work around the FF < 36 issue

--- matchMedia.js
+++ matchMedia.js
@@ -44,3 +44,78 @@ window.matchMedia || (window.matchMedia = function() {
         };
     };
 }());
+
+(function() {
+    "use strict";
+
+    if(window.matchMedia && window.matchMedia('all') === null) {
+        // this is FF < 36 in a hidden iframe - fake calls until we get displayed
+        var realMatchMedia = window.matchMedia;
+        var log = function() { if(window.console && console.log){ console.log.apply(console, arguments);    }};
+        var listeners = {};
+        var handleResize = function handleResize() {
+            if(realMatchMedia('all') === null) {
+                return;
+            }
+            // ok, we're visible again, call and move the listeners we've cached over to the real thing
+            log('got a non-null return from the browser matchMedia call - copying listeners over');
+            var toRemove = [];
+            var i;
+            var removeHandler = true;
+            for(var key in listeners) {
+                var r = realMatchMedia(key);
+                if(r !== null) {
+                    for(i=0; i<listeners[key].length; i++) {
+                        r.addListener(listeners[key][i]);
+                        log('delegated for ' + key + ' to', listeners[key][i]);
+                        listeners[key][i].call(window, r);
+                    }
+                    toRemove.push(key);
+                } else {
+                    removeHandler = false;
+                }
+            }
+            for(i=0; i<toRemove.length; i++) {
+                delete listeners[toRemove[i]];
+            }
+
+            if(removeHandler && isHandlingResize){
+                window.removeEventListener('resize', handleResize, true);
+                isHandlingResize = false;
+                log('disabled resize handler');
+            }
+        };
+        var isHandlingResize = false;
+
+        window.matchMedia = function(media) {
+            var r = realMatchMedia(media);
+            if(r !== null) {
+                return r;
+            }
+            log('faking matchMedia for ' + media);
+
+            // ok, now our fix comes in...
+            r = { matches: false };
+            r.addListener = function(callback) {
+                if(!isHandlingResize){
+                    window.addEventListener('resize', handleResize, true);
+                    isHandlingResize = true;
+                    log('enabled resize handler');
+                }
+
+                listeners[media] = listeners[media] || [];
+                listeners[media].push(callback);
+            };
+            r.removeListener = function(callback) {
+                listeners[media] = listeners[media] || [];
+                for(var i=listeners[media].length-1; i>=0; i--) {
+                    if(listeners[media][i] === callback) {
+                        listeners[media].splice(i, 1);
+                    }
+                }
+            }
+            return r;
+        };
+    }
+})();
+
